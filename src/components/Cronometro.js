31dcc2d7733bd8ca29537bbd27c6c58febb16080
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-07-03 18:04:31 -0300

    initial commit - react app

---
+++ src/components/Cronometro.js
@@ -0,0 +1,45 @@
+import React, {useState, useRef, useCallback} from "react";
+
+const Cronometro = () => {
+
+    const [segundos, setSegundos] = useState(0);
+    const[isActive, setIsActive] = useState(false);
+    const intervalRef = useRef(null);
+
+    const iniciarCronometro = () => {
+        setIsActive(true);
+        intervalRef.current = setInterval(() => {
+            setSegundos(segundos => segundos + 1);
+            }, 1000);
+
+
+
+    };
+    const pausarCronometro = () => {
+        clearInterval(intervalRef.current);
+        setIsActive(false);
+    };
+    const zerarCronometro = () => {
+        clearInterval(intervalRef.current);
+        setIsActive(false);
+        setSegundos(0);
+
+
+    };
+    return(
+        <div className="cronometro">
+            <h1>{segundos}s</h1>
+            <div className="botoes">
+                {!isActive? (
+                    <button onClick={iniciarCronometro}>Iniciar</button>
+                )
+                : (<button onClick={pausarCronometro}>Iniciar</button>
+                )}
+                <button onClick={zerarCronometro}>Zerar</button>
+            </div>
+            
+        </div>
+    );
+
+}
+export default Cronometro;
