5426ae4e7d247ed800fe0e4b91f1b5c5930cc7b1
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-07-04 15:06:54 -0300

    new fixes and html code on jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Cronometro from './Cronometro.js';
+import Cronometro from './components/Cronometro';
 
 export default function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Cronômetro</h1>
+                <h1>Cronometro</h1>
+                <h2>Teste de Cronometro</h2>
+                <img src="<a href=https://www.flaticon.com/br/icones-gratis/bruxa"/>
             </header>
             <main>
                 <Cronometro />
